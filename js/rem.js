dec74bf6de206f85c4034c81c215062767ef0b70
remnia <732893598@qq.com>
2019-02-20 02:04:40 +0800

    first commit

---
+++ js/rem.js
@@ -0,0 +1,19 @@
+let width = document.documentElement.clientWidth;
+if(width > 640){
+	width = 640;
+}
+document.documentElement.style.fontSize=width/750*100+"px";
+
+//window.onload = function(){
+//  /*720代表设计师给的设计稿的宽度，你的设计稿是多少，就写多少;100代表换算比例，这里写100是
+//    为了以后好算,比如，你测量的一个宽度是100px,就可以写为1rem,以及1px=0.01rem等等*/
+//  getRem(720,100)
+//};
+//window.onresize = function(){
+//  getRem(720,100)
+//};
+//function getRem(pwidth,prem){
+//  var html = document.getElementsByTagName("html")[0];
+//  var oWidth = document.body.clientWidth || document.documentElement.clientWidth;
+//  html.style.fontSize = oWidth/pwidth*prem + "px";
+//}
